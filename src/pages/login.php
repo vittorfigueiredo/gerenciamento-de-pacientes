e3acef0a215819df49e302dc890f76fdc52a3d52
vitorfigueiredopb <vitorfigueiredo855@gmail.com>
2020-12-04 17:15:50 -0500

    criação dos diretórios e arquivos que os compõe

---
+++ src/pages/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Login</title>
+</head>
+<body>
+
+    <header>
+        <h1 class="titulo">Fazer Login</h1>
+    </header>
+
+    <div class="container">
+
+        <form action="">
+            <div class="email">
+                <label>Email:</label>
+                <input type="text">
+            </div>
+            <div class="senha">
+                <label>Senha:</label>
+                <input type="password">
+            </div>
+
+            <button>Cadastrar</button>
+        </form>
+
+    </div>
+
+    <footer>©Copyright 2020 - Todos os direitos reservados!</footer>
+
+</body>
+</html>
